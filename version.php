eb73a2bd31ec490cd2884b22d9a831520c08413c
Eduardo Kraus <kraus@eduardokraus.com>
2025-09-02 20:15:40 -0300

    V 9.1.0

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2025090300;
 $plugin->requires = 2022010100;
-$plugin->release = "9.0.9";
+$plugin->release = "9.1.0";
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'theme_eadtraining';
 $plugin->dependencies = [
