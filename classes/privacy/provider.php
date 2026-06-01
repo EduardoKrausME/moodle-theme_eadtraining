c936875f5415e24aed7be660159f9eaf490ddf08
Eduardo Kraus <kraus@eduardokraus.com>
2026-05-31 21:17:00 -0300

    disable Universal.OOStructures.AlphabeticExtendsImplements.ImplementsWrongOrder

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -15,6 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * phpcs:disable Universal.OOStructures.AlphabeticExtendsImplements.ImplementsWrongOrder
  * Privacy Subsystem implementation for theme_eadtraining.
  *
  * @package   theme_eadtraining
